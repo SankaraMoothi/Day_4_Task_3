d542c637558db165e4fe205af561545bc5295c84
Sankara_Moorthi <107635975+SankaraMoothi@users.noreply.github.com>
2022-06-29 12:51:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/all', true);
+
+request.send();
+request.onload = function () {
+    var data = JSON.parse(request.response);
+    console.log(data);
+
+    for (var j in data) {
+        console.log(data[j].name,data[j].region,data[j].sub-region,dat[j].population);
+    }
+    
+}
